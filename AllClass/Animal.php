df827ab8e5ae26d15a53d03bc9286dd12ab172ba
JLTYW102\admin <jones0716@gmail.com>
2017-11-01 19:25:20 +0800

    把 動物共同會做事 放在父類別

--- AllClass/Animal.php
+++ AllClass/Animal.php
@@ -9,6 +9,7 @@ class Animal
 {
     private $animalType = "default animal Type";//若沒有呼叫父類別的建構子,會印出這個
     protected $position;
+    protected $foodName;
 
     public function __construct($animalTypeFromOut)     //建構子   通常用來初始化動作
     {
@@ -21,9 +22,21 @@ class Animal
         return $this->animalType;
     }
 
-    public function getFood($footName)
-    {
+    public function getFood($footName) {
+	$this->foodName = $foodName;
         echo "得到食物: ".$footName."<br>";
+	//判斷不要吃
+	$this->eat();
+    }
+
+    Protected function eat() {
+	eat "吃".$this->foodName."<br>";
+
+	$this->digest();
+    }
+
+    Private function digest() {
+	echo "消化".$this->foodName."<br>";
     }
 }
 
--- AllClass/Cat.php
+++ AllClass/Cat.php
@@ -9,12 +9,16 @@ class Cat extends Animal
     public function __construct($petNameFromOut) {
 	parent::__construct("貓");//需要呼叫父類別初始化的方法,否則父類別不會做
         $this->petName = $petNameFromOut;
+	$this->position;
     }
 
     public function getPetName() {
+//	  $this->getAnimalType();//測試 protected 方法
         return $this->petName;
     }
 }
 
 $cat = new Cat("Kitty");
 echo $cat->getAnimalType();//繼承後可以直接使用父類別的 public 方法
+
+$cat->getFood("小魚");
